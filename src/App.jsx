fb8c4fd2cb68ac8b21c42070a157c89da869e187
SergeyBildanov <troktrokov@gmail.com>
2024-11-30 23:52:51 +0400

    app changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ function App() {
       <Card img={"src/assets/nature.jpeg"}>
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <p className="card-text">{"Some quick example text to build on the card title and make up the bulk of the card's content."}</p>
           <a href="#" className="btn btn-primary">Go somewhere</a>
         </div>
       </Card>
       <Card>
         <div className="card-body">
           <h5 className="card-title">Another Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <p className="card-text">{"Some quick example text to build on the card title and make up the bulk of the card's content."}</p>
           <a href="#" className="btn btn-primary">Go somewhere</a>
         </div>
       </Card>
