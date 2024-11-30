edd32ad38a58e9f8fadddebec8d2987720b1bac8
SergeyBildanov <troktrokov@gmail.com>
2024-12-01 00:38:16 +0400

    picture changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Card from './components/card'
 function App() {
   return (
     <div className="card-wrapper">
-      <Card img={"./nature.jpeg"}>
+      <Card img={'src/nature.jpeg'}>
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
           <p className="card-text">{"Some quick example text to build on the card title and make up the bulk of the card's content."}</p>
