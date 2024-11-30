63b410acff847a46105c856284b26890eab4f6d1
SergeyBildanov <troktrokov@gmail.com>
2024-12-01 00:01:42 +0400

    card logic changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Card from './components/card'
 function App() {
   return (
     <div className="card-wrapper">
-      <Card img={"src/assets/nature.jpeg"}>
+      <Card img={"assets/nature.jpeg"}>
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
           <p className="card-text">{"Some quick example text to build on the card title and make up the bulk of the card's content."}</p>
